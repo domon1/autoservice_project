a2e9102016cb338be8f20a0e1fbbd2b3c0f5e957
domoni <dimoniy.dota2@mail.ru>
2022-12-15 13:07:26 +0300

    add service manager actions

--- course_test/Controllers/ManagerController.cs
+++ course_test/Controllers/ManagerController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BusinessLayer;
+using DataLayer.Entityes;
 
 namespace course_test.Controllers
 {
@@ -16,14 +17,75 @@ namespace course_test.Controllers
             _dataManager = dataManager;
         }
 
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
 
+        [HttpGet]
         public IActionResult Services()
         {
             return View(_dataManager.CarService.GetAll());
+        }
+
+        [HttpGet]
+        public IActionResult AddService()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddService(CarService carService)
+        {
+            _dataManager.CarService.Create(carService);
+            _dataManager.CarService.Save();
+            return RedirectToAction("Services");
+        }
+
+        [HttpGet]
+        public IActionResult EditService(int id)
+        {
+            CarService carService = _dataManager.CarService.GetById(id);
+            if (carService != null)
+            {
+                return View(carService);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost]
+        public IActionResult EditService(CarService carService)
+        {
+            _dataManager.CarService.Update(carService);
+            _dataManager.CarService.Save();
+            return RedirectToAction("Services");
+        }
+
+        [HttpGet]
+        [ActionName("DeleteService")]
+        public IActionResult ConfirmDeleteService(int id)
+        {
+            CarService carService = _dataManager.CarService.GetById(id);
+            if (carService != null)
+            {
+                return View(carService);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost]
+        public IActionResult DeleteService(int id)
+        {
+            _dataManager.CarService.Delete(id);
+            _dataManager.CarService.Save();
+            return RedirectToAction("Services");
         }
     }
 }
